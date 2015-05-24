747a8fb790fbebd7ed5da22b1c36287554e10089
Kamil Kiewisz <kamkie@outlook.com>
2015-05-24 20:25:57 +0200

    sinus generator demo, slower

--- jni-client/src/main/java/net/devops/demo/jni/client/SinusGeneratorDemo.java
+++ jni-client/src/main/java/net/devops/demo/jni/client/SinusGeneratorDemo.java
@@ -3,10 +3,8 @@ package net.devops.demo.jni.client;
 import lombok.extern.slf4j.Slf4j;
 import net.devops.demo.jni.bindings.SinusGenerator;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
 
 @Slf4j
 public class SinusGeneratorDemo {
@@ -14,38 +12,29 @@ public class SinusGeneratorDemo {
     public static final int SIN_SIZE = 1_000_000;
     public static final int NUMBER_OF_ITERATIONS = 10;
     private SinusGenerator sinusGenerator = new SinusGenerator();
+    ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(500);
+    ExecutorService executorService = new ThreadPoolExecutor(50, 100, 100, TimeUnit.SECONDS, workQueue);
 
     public SinusGeneratorDemo() throws Exception {
-        ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(500);
-        ExecutorService executorService = new ThreadPoolExecutor(50, 100, 100, TimeUnit.SECONDS, workQueue);
-        executorService.execute(() -> {
+        Future<?> future = executorService.submit(() -> {
             sinusGenerator.cppOpenMp(100);
             sinusGenerator.cppSequential(100);
             sinusGenerator.javaMath(100);
             sinusGenerator.javaFastMath(100);
         });
-        executorService.shutdown();
-        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        future.get();
     }
 
     public void testCppOpenMp() throws Exception {
-        ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(500);
-        ExecutorService executorService = new ThreadPoolExecutor(50, 100, 100, TimeUnit.SECONDS, workQueue);
         long startTime = System.currentTimeMillis();
         log.debug("testCppOpenMp: start time= {}", startTime);
 
-        for (int i = 0; i < NUMBER_OF_ITERATIONS; i++) {
-            final int loop = i;
-            executorService.execute(() -> {
-                long start = System.currentTimeMillis();
-                double[] sinus = sinusGenerator.cppOpenMp(SIN_SIZE);
-                long time = System.currentTimeMillis() - start;
-                log.info("testCppOpenMp: i: {}\ttime: {}", loop, time);
-            });
-        }
-
-        executorService.shutdown();
-        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        IntStream.range(0, NUMBER_OF_ITERATIONS).mapToObj(i -> executorService.submit(() -> {
+            long start = System.currentTimeMillis();
+            double[] sinus = sinusGenerator.cppOpenMp(SIN_SIZE);
+            long time = System.currentTimeMillis() - start;
+            log.info("testCppOpenMp: i: {}\ttime: {}", i, time);
+        })).forEach(this::safeGetFuture);
 
         long stopTime = System.currentTimeMillis();
         log.debug("testCppOpenMp: stop time={}", stopTime);
@@ -53,23 +42,15 @@ public class SinusGeneratorDemo {
     }
 
     public void testCppSequential() throws Exception {
-        ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(500);
-        ExecutorService executorService = new ThreadPoolExecutor(50, 100, 100, TimeUnit.SECONDS, workQueue);
         long startTime = System.currentTimeMillis();
         log.debug("testCppSequential: start time= {}", startTime);
 
-        for (int i = 0; i < 10; i++) {
-            final int loop = i;
-            executorService.execute(() -> {
-                long start = System.currentTimeMillis();
-                double[] sinus = sinusGenerator.cppSequential(SIN_SIZE);
-                long time = System.currentTimeMillis() - start;
-                log.info("testCppSequential: i: {}\ttime: {}", loop, time);
-            });
-        }
-
-        executorService.shutdown();
-        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        IntStream.range(0, NUMBER_OF_ITERATIONS).mapToObj(i -> executorService.submit(() -> {
+            long start = System.currentTimeMillis();
+            double[] sinus = sinusGenerator.cppSequential(SIN_SIZE);
+            long time = System.currentTimeMillis() - start;
+            log.info("testCppSequential: i: {}\ttime: {}", i, time);
+        })).forEach(this::safeGetFuture);
 
         long stopTime = System.currentTimeMillis();
         log.debug("testCppSequential: stop time={}", stopTime);
@@ -77,51 +58,42 @@ public class SinusGeneratorDemo {
     }
 
     public void testJavaMath() throws Exception {
-        ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(500);
-        ExecutorService executorService = new ThreadPoolExecutor(50, 100, 100, TimeUnit.SECONDS, workQueue);
         long startTime = System.currentTimeMillis();
         log.debug("testJavaMath: start time= {}", startTime);
 
-        for (int i = 0; i < 10; i++) {
-            final int loop = i;
-            executorService.execute(() -> {
-                long start = System.currentTimeMillis();
-                double[] sinus = sinusGenerator.javaMath(SIN_SIZE);
-                long time = System.currentTimeMillis() - start;
-                log.info("testJavaMath: i: {}\ttime: {}", loop, time);
-            });
-        }
-
-        executorService.shutdown();
-        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        IntStream.range(0, NUMBER_OF_ITERATIONS).mapToObj(i -> executorService.submit(() -> {
+            long start = System.currentTimeMillis();
+            double[] sinus = sinusGenerator.javaMath(SIN_SIZE);
+            long time = System.currentTimeMillis() - start;
+            log.info("testJavaMath: i: {}\ttime: {}", i, time);
+        })).forEach(this::safeGetFuture);
 
         long stopTime = System.currentTimeMillis();
         log.debug("testJavaMath: stop time={}", stopTime);
         log.info("testJavaMath: time={}\n", stopTime - startTime);
     }
 
-
     public void testJavaFastMath() throws Exception {
-        ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(500);
-        ExecutorService executorService = new ThreadPoolExecutor(50, 100, 100, TimeUnit.SECONDS, workQueue);
         long startTime = System.currentTimeMillis();
         log.debug("testJavaFastMath: start time= {}", startTime);
 
-        for (int i = 0; i < 10; i++) {
-            final int loop = i;
-            executorService.execute(() -> {
-                long start = System.currentTimeMillis();
-                double[] sinus = sinusGenerator.javaFastMath(SIN_SIZE);
-                long time = System.currentTimeMillis() - start;
-                log.info("testJavaFastMath: i: {}\ttime: {}", loop, time);
-            });
-        }
-
-        executorService.shutdown();
-        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        IntStream.range(0, NUMBER_OF_ITERATIONS).mapToObj(i -> executorService.submit(() -> {
+            long start = System.currentTimeMillis();
+            double[] sinus = sinusGenerator.javaFastMath(SIN_SIZE);
+            long time = System.currentTimeMillis() - start;
+            log.info("testJavaFastMath: i: {}\ttime: {}", i, time);
+        })).forEach(this::safeGetFuture);
 
         long stopTime = System.currentTimeMillis();
         log.debug("testJavaFastMath: stop time={}", stopTime);
         log.info("testJavaFastMath: time={}\n", stopTime - startTime);
+    }
+
+    private void safeGetFuture(Future future) {
+        try {
+            future.get();
+        } catch (Exception e) {
+            log.error("", e);
+        }
     }
 }
